49d9b75292f670706bba41d89f4a2a3ad95f2c57
dwilliams64 <dwilliams_64@yahoo.com>
2019-01-13 13:36:51 -0600

    trying new solution

--- 214_Async_Await/exerciseAsyncAwait.js
+++ 214_Async_Await/exerciseAsyncAwait.js
@@ -5,6 +5,14 @@ fetch('https://swapi.co/api/starships/9/')
   .then(response => response.json())
   .then(console.log)
 
+//Solution
+async function fetchData(){
+  const reqs = await fetch('https://swapi.co/api/starships/9/');
+  const data = await reqs.json();
+  console.log(data);
+}
+
+fetchData();
 
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
@@ -24,6 +32,28 @@ const getData = async function() {
   console.log('posta', posts);
   console.log('albums', albums);
 }
+
+//Solution 1
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(data(url)));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+
+async function data(url){
+  const reqs = await fetch(url);
+  const data = await reqs.json();
+  return data;
+}
+
+getData();
 
 // #3) Add a try catch block to the #2 solution in order to catch any errors.
 // Now chnage one of the urls so you console.log your error with 'ooooooops'

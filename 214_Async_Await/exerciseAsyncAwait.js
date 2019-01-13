e8e3c2675ff3d6c30a96e6a93c00ed0d2ccb77fc
dwilliams64 <dwilliams_64@yahoo.com>
2019-01-13 13:11:28 -0600

    re doing exercise

--- 214_Async_Await/exerciseAsyncAwait.js
+++ 214_Async_Await/exerciseAsyncAwait.js
@@ -5,13 +5,6 @@ fetch('https://swapi.co/api/starships/9/')
   .then(response => response.json())
   .then(console.log)
 
-async function getStarship(){
-  const resp = await fetch('https://swapi.co/api/starships/9/');
-  const data = await resp.json();
-  console.log(data);
-}
-
-getStarship();
 
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
@@ -32,62 +25,5 @@ const getData = async function() {
   console.log('albums', albums);
 }
 
-
-const urls = [
-  'https://jsonplaceholder.typicode.com/users',
-  'https://jsonplaceholder.typicode.com/posts',
-  'https://jsonplaceholder.typicode.com/albums'
-]
-
-const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(url =>{
-    return grabData(url);
-  }      
-
-  ));
-  console.log('users', users);
-  console.log('posts', posts);
-  console.log('albums', albums);
-}
-
-async function grabData(url){
-  const resp = await fetch(url);
-  const data = await resp.json();  
-  return data        
-}
-
-
-getData();
-
 // #3) Add a try catch block to the #2 solution in order to catch any errors.
 // Now chnage one of the urls so you console.log your error with 'ooooooops'
-
-const urls = [
-  'https://jsonplaceholder.typicode/users',
-  'https://jsonplaceholder.typicode.com/posts',
-  'https://jsonplaceholder.typicode.com/albums'
-]
-
-const getData = async function() {
-  try{
-    const [ users, posts, albums ] = await Promise.all(urls.map(url =>{
-      return grabData(url);
-    }      
-  
-    ));
-    console.log('users', users);
-    console.log('posts', posts);
-    console.log('albums', albums);
-
-    async function grabData(url){
-      const resp = await fetch(url);
-      const data = await resp.json();  
-      return data        
-    }
-
-  } catch(e) {
-    console.log('oooooooopss: ', e);
-  }
-}  
-
-getData();

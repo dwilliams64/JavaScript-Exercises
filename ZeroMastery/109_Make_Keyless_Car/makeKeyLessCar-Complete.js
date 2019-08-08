cd6f3d9d1ad8e5f1dfb51c188d0174b97575e239
dwilliams64 <dwilliams_64@yahoo.com>
2019-08-08 07:51:37 -0500

    Completed Keyless care exercise

--- ZeroMastery/109_Make_Keyless_Car/makeKeyLessCar-Complete.js
+++ ZeroMastery/109_Make_Keyless_Car/makeKeyLessCar-Complete.js
@@ -12,6 +12,55 @@
 // IF they say they are over 18, respond with:
 // "Powering On. Enjoy the ride!"
 
+
+//ES5 Solution without converter function
+var age = prompt("What is thy age?");
+
+function numChk(age) {
+  if(isNaN(age)){
+    console.log("Please enter a numerical value")
+  } else {
+    ageChk(age);
+  }
+}
+
+function ageChk(age) {
+  if(Number(age) < 18) {
+    console.log("Sorry, you are too young to drive this car. Powering off");
+  } else if(Number(age) === 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+  } else if(Number(age) > 18) {
+    console.log("Powering On. Enjoy the ride!");
+  }
+}
+
+numChk(age);
+
+
+//ES6 Solution with converter function
 let age = prompt("What is thy age?");
 
-console.log(age);
+const numChk = age => {
+  if(isNaN(age)){
+    console.log("Please enter a numerical value")
+  } else {
+    numConvert(age);
+  }
+}
+
+const numConvert = age => {
+  age = Number(age);
+  ageChk(age);
+}
+
+const ageChk = age => {
+  if(age < 18) {
+    console.log("Sorry, you are too young to drive this car. Powering off");
+  } else if(age === 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+  } else if(age > 18) {
+    console.log("Powering On. Enjoy the ride!");
+  }
+}
+
+numChk(age);

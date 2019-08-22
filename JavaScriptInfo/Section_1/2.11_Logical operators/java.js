4e9f877a1e98f7e7cc26ef37c865c8ee167fabfb
dwilliams64 <dwilliams_64@yahoo.com>
2019-08-22 16:52:37 -0500

    Upadated 2.10 Logical operators

--- JavaScriptInfo/Section_1/2.11_Logical operators/java.js
+++ JavaScriptInfo/Section_1/2.11_Logical operators/java.js
@@ -75,6 +75,104 @@ if(age >= 14 || age <= 90)
 // Write an if condition to check that age is NOT between 14 and 90 inclusively.
 // Create two variants: the first one using NOT !, the second one – without it.
 
-if()
-
+if(!(age >= 14 && age <= 90)) 
+  
 if(age < 14 || age > 90)
+
+// Which of these alerts are going to be executed?
+// What will the results of the expressions be inside if(...)
+
+if (-1 || 0) alert('first'); // will run | -1 truthy
+if (-1 && 0) alert('second'); // will not run | 0 falsy
+if (null || -1 && 1) alert('third'); // will run | 1 truthy
+
+/*
+
+The answer: the first and the third will execute.
+
+Details:
+
+ // Runs.
+// The result of -1 || 0 = -1, truthy
+if (-1 || 0) alert( 'first' );
+
+// Doesn't run
+// -1 && 0 = 0, falsy
+if (-1 && 0) alert( 'second' );
+
+// Executes
+// Operator && has a higher precedence than ||
+// so -1 && 1 executes first, giving us the chain:
+// null || -1 && 1  ->  null || 1  ->  1
+if (null || -1 && 1) alert( 'third' );
+
+*/
+
+
+
+/*
+
+Write the code which asks for a login with prompt.
+
+If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+
+The password is checked as follows:
+
+If it equals “TheMaster”, then show “Welcome!”,
+Another string – show “Wrong password”,
+For an empty string or cancelled input, show “Canceled”
+
+*/
+
+
+
+const login = () => {
+    let userName = prompt("Please enter user name");
+
+    if(userName == "Admin") {
+        password();
+    } else if (userName == null || userName == "") {
+        alert("Cancel");
+    } else {
+        alert("I don't know you");
+    }
+}
+
+const password = () => {
+    let password = prompt("Please enter password");
+
+    if(password == "TheMaster") {
+        alert("Welcome");
+    } else if(password == null || password == "") {
+        alert("Cancel");
+    } else {
+        alert("Wrong password");
+    }
+}
+
+login();
+
+/*
+
+let userName = prompt("Who's there?", '');
+
+if (userName == 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass == 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass == '' || pass == null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName == '' || userName == null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
+Note the vertical indents inside the if blocks. They are technically not required, but make the code more readable.
+
+*/

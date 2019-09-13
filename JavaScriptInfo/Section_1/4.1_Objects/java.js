8890c498b0f1f8feb3b39401b7d90beb239d2de7
dwilliams64 <dwilliams_64@yahoo.com>
2019-09-13 18:32:18 -0500

    Added new exercises, updated 5.2_Numbers, Completed 4.1_Object

--- JavaScriptInfo/Section_1/4.1_Objects/java.js
+++ JavaScriptInfo/Section_1/4.1_Objects/java.js
@@ -1,6 +1,6 @@
 /*
 
-Problem:
+Q1:
 
 Write the code, one line for each action:
 
@@ -37,7 +37,7 @@ delete user.name;
 
 /*
 
-Problem:
+Q2:
 
 Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
 
@@ -53,6 +53,8 @@ alert( isEmpty(schedule) ); // false
 
 */
 
+// My solution:
+
 let schedule = {};
 
 alert( isEmpty(schedule) ); // true
@@ -62,13 +64,190 @@ schedule["8:30"] = "get up";
 alert( isEmpty(schedule) ); // false
 
 function isEmpty(obj) {
-    for (let prop in obj) {
-        if (obj[prop] == undefined) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+    for (let key in obj) {
     
+      return false;
+    }
+
+    return true;
 }
 
+/*
+
+JavaScript.info:
+
+Just loop over the object and return false immediately if there’s at least one property.
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
+
+*/
+
+
+
+
+/*
+
+Q3:
+
+Is it possible to change an object declared with const? What do you think?
+
+const user = {
+  name: "John"
+};
+
+// does it work?
+user.name = "Pete";
+
+*/
+
+/*
+
+My answer:
+
+Yes it works because the constant is only storing a refernce to the object.
+
+Changing the object itself will not effect the constant because we are not changing
+the constant value itself just the object reference
+
+JavaScript.info:
+
+Sure, it works, no problem.
+
+The const only protects the variable itself from changing.
+
+In other words, user stores a reference to the object. And it can’t be changed. But the content of the object can.
+
+const user = {
+  name: "John"
+};
+
+// works
+user.name = "Pete";
+
+// error
+user = 123;
+
+*/
+
+/*
+
+Q4:
+
+We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0.
+
+*/
+
+// My solution
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+  
+  let sum = 0;
+  
+  for (let key in salaries) {
+     sum += salaries[key];
+  }
+  
+  console.log(sum);
+
+  /*
+
+  JavaScript.info:
+
+  let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+alert(sum); // 390
+
+  */
+
+
+/*
+
+Q5:
+
+Create a function multiplyNumeric(obj) that multiplies all numeric properties of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+P.S. Use typeof to check for a number here.
+
+*/
+
+// My solution
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === "number") {
+        obj[key] * 2;
+      } 
+    }
+  }
+  
+  multiplyNumeric(menu);
+  
+  console.log(menu);
+
+  /*
+
+  JavaScript.info:
+
+  function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == 'number') {
+      obj[key] *= 2;
+    }
+  }
+}
+
+*/
--- JavaScriptInfo/Section_1/5.2_Numbers/java.js
+++ JavaScriptInfo/Section_1/5.2_Numbers/java.js
@@ -8,6 +8,36 @@ Check demo for requriments.
 
 */
 
+// My Solution 1:
+
+let num1 = prompt("Enter a number");
+
+let num2 = prompt("Enter another number");
+
+if (Object.is(parseFloat(num1), NaN)  || Object.is(parseFloat(num2), NaN)) {
+  alert(NaN);
+} else {
+  alert(Number(num1) + Number(num2));
+
+// Note: My solution will alert NaN if null is a value
+
+/*
+
+JavaScript.info:
+
+let a = +prompt("The first number?", "");
+let b = +prompt("The second number?", "");
+
+alert( a + b );
+
+Note the unary plus + before prompt. It immediately converts the value to a number.
+
+Otherwise, a and b would be string their sum would be their concatenation, that is: "1" + "2" = "12".
+
+
+*/
+
+
 
 
 
@@ -42,6 +72,56 @@ The visitor can also stop the process by entering an empty line or pressing “C
 
 */
 
+//My solution:
+
+function readNumber() {
+  let num;
+
+  do {
+    num = prompt("Please enter a number", "0");
+    console.log(`top: ${num}`);
+    if (num === null || num.length === 0) {
+      return alert(`Read: null`);
+    }
+  
+    if ( parseFloat(num) || +num === 0 ) {
+      return alert(`Read: ${num}`);
+    }
+  
+    
+    console.log(`bottom: ${num}`);
+  } while (isNaN(num));  
+}
+
+readNumber();
+
+// My solution doe not cover empty strings with big gaps like "   ".
+// Use isFinite to check for numbers to cover this case
+
+/*
+
+JavaScript.info:
+
+function readNumber() {
+  let num;
+
+  do {
+    num = prompt("Enter a number please?", 0);
+  } while ( !isFinite(num) );
+
+  if (num === null || num === '') return null;
+
+  return +num;
+}
+
+alert(`Read: ${readNumber()}`);
+The solution is a little bit more intricate that it could be because we need to handle null/empty lines.
+
+So we actually accept the input until it is a “regular number”. Both null (cancel) and empty line also fit that condition, because in numeric form they are 0.
+
+After we stopped, we need to treat null and empty line specially (return null), because converting them to a number would return 0.
+
+*/
 
 
 /*
@@ -73,6 +153,36 @@ alert( random(1, 5) ); // 4.3435234525
 
 */
 
+// My solution:
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+random(1, 5);
+
+/*
+
+JavaScript.info:
+
+We need to “map” all values from the interval 0…1 into values from min to max.
+
+That can be done in two stages:
+
+If we multiply a random number from 0…1 by max-min, then the interval of possible values increases 0..1 to 0..max-min.
+Now if we add min, the possible interval becomes from min to max.
+The function:
+
+ function random(min, max) {
+  return min + Math.random() * (max - min);
+}
+
+alert( random(1, 5) );
+alert( random(1, 5) );
+alert( random(1, 5) );
+
+*/
+
 
 
 /*
@@ -92,4 +202,72 @@ You can use the solution of the previous task as the base.
 
 */
 
+// My solution
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+random(1, 5);
+
+/*
+
+JavaScript.info:
+
+The simple but wrong solution
+The simplest, but wrong solution would be to generate a value from min to max and round it:
+
+ function randomInteger(min, max) {
+  let rand = min + Math.random() * (max - min);
+  return Math.round(rand);
+}
+
+alert( randomInteger(1, 3) );
+The function works, but it is incorrect. The probability to get edge values min and max is two times less than any other.
+
+If you run the example above many times, you would easily see that 2 appears the most often.
+
+That happens because Math.round() gets random numbers from the interval 1..3 and rounds them as follows:
+
+values from 1    ... to 1.4999999999  become 1
+values from 1.5  ... to 2.4999999999  become 2
+values from 2.5  ... to 2.9999999999  become 3
+Now we can clearly see that 1 gets twice less values than 2. And the same with 3.
+
+The correct solution
+There are many correct solutions to the task. One of them is to adjust interval borders. To ensure the same intervals, we can generate values from 0.5 to 3.5, thus adding the required probabilities to the edges:
+
+ 
+
+
+
+
+function randomInteger(min, max) {
+  // now rand is from  (min-0.5) to (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
+alert( randomInteger(1, 3) );
+An alternative way could be to use Math.floor for a random number from min to max+1:
+
+ 
+
+
+
+
+function randomInteger(min, max) {
+  // here rand is from min to (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+alert( randomInteger(1, 3) );
+Now all intervals are mapped this way:
+
+values from 1  ... to 1.9999999999  become 1
+values from 2  ... to 2.9999999999  become 2
+values from 3  ... to 3.9999999999  become 3
+All intervals have the same length, making the final distribution uniform.
+
+*/

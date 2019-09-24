574742a96736971b3a4df23d8bf3e418f157394b
dwilliams64 <dwilliams_64@yahoo.com>
2019-09-24 10:32:23 -0500

    Started JavaScript bootcamp

---
+++ js-bootcamp/basics/numbers.js
@@ -0,0 +1,24 @@
+// Challenge
+
+/*
+
+Calculate the student score with the following 
+
+studentScore 18
+
+maxScore 20
+
+studentScore / maxScore * 100
+
+percent 90 (above formula must equal 90 when using 18 and 20)
+
+*/
+
+let studentScore = 18;
+
+let maxScore = 20;
+
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent + "%");
+
---
+++ js-bootcamp/basics/strings.js
@@ -0,0 +1,20 @@
+// Sting and Variable Challenge
+
+/*
+
+Create the following variables:
+
+city (store city)
+
+country (store country)
+
+locaton (should read city, country)
+*/
+
+let city = "San Antonio";
+
+let country = "US of A";
+
+let location = city + ", " + country;
+
+console.log(location);

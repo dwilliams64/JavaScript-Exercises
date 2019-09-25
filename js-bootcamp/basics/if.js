c8b2f64ded49bbd7b94f2678fbc447b0d6d5052a
dwilliams64 <dwilliams_64@yahoo.com>
2019-09-25 12:14:25 -0500

    completed if statement exercise

---
+++ js-bootcamp/basics/if.js
@@ -0,0 +1,17 @@
+// Challenge
+
+// Create age set to your age
+// Calculate is child - if they are 7 or under
+// Calculate is senior - if they are 65 or older
+// Print is child value
+// Print is senior value
+
+let age = 95;
+
+if (age <= 7) {
+    console.log('you get the child pricing');
+}
+
+if (age >= 65) {
+    console.log('you get the senior pricing')
+}

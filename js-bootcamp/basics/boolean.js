3647dca9bfb1b907e48751503a4e6949c5a92627
dwilliams64 <dwilliams_64@yahoo.com>
2019-09-25 11:24:46 -0500

    Completed boolean exercise

---
+++ js-bootcamp/basics/boolean.js
@@ -0,0 +1,17 @@
+// Challenge
+
+// Create age set to your age
+// Calculate is child - if they are 7 or under
+// Calculate is senior - if they are 65 or older
+// Print is child value
+// Print is senior value
+
+let age = 35;
+
+let isChild = age <= 7;
+
+let isSenior = age >= 65;
+
+console.log(`Is child: ${isChild}`);
+
+console.log(`Is senior: ${isSenior}`);

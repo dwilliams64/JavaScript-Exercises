f3584662b5a4bbbe283d6dfa6b42e95272938a23
dwilliams64 <dwilliams_64@yahoo.com>
2019-10-02 14:14:50 -0500

    Completed arrays challenge, added array.js, added, notes.js, added todo.js

---
+++ js-bootcamp/arrays/todo.js
@@ -0,0 +1,9 @@
+// Create an array with five todos
+// Print message: You have x todos (x is the length of array)
+// Print the first and second to last itmes -> Todo: walk the dog
+
+const todos = ['study JavaScript', 'finish Sass exercise', 'complete SVG exercise', 'feed the monkey', 'beat mega man x9'];
+
+console.log(`You have ${todos.length} todos!`);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`)

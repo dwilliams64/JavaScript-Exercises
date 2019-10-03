46d9145b27178423d8a97f1176f2d34d6cffc46e
dwilliams64 <dwilliams_64@yahoo.com>
2019-10-03 08:59:15 -0500

    Completed looping over arrays and manipulating arrays with methods

--- js-bootcamp/arrays/array.js
+++ js-bootcamp/arrays/array.js
@@ -1,6 +1,66 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// Get length of array (how many items in array)
 console.log(notes.length); // 3
+
+
+// Accessing items in array
 console.log(notes[0]); // Note 1
 console.log(notes[22]); // undefined
 console.log(notes[notes.length - 1]); // Note 3
+
+console.log('\n');
+
+// Add items to end of array
+console.log('Push: ', notes.push('Note 4'));
+
+console.log('Push: ', notes);
+console.log('\r');
+
+// Remove last item of an array
+console.log('Pop: ', notes.pop()); // returns the item that is being removed. We can store this item in a variable for later use if we want
+
+console.log('Pop: ', notes);
+console.log('\r');
+
+// Removes item at the begining of an array
+console.log('Shift: ', notes.shift()); // returns the item that is being removed. We can store this item in a variable for later use if we want
+
+console.log('Shift: ', notes);
+console.log('\r');
+
+// Adds item to begining of array
+console.log('UnShift: ', notes.unshift('Note 0'));
+
+console.log('UnShift: ',notes);
+console.log('\r');
+
+// Removes item in array. First argument is the position. Second is how many items you want to remove
+console.log('Splice Remove: ', notes.splice(1, 1));
+
+console.log('Splice Remove: ', notes);
+console.log('\r');
+
+// Adds item to an array. First argumen is the position. Second is how many items to be removed (0 means no items to be removed)
+// Last argument is what we want to add.
+console.log('Splice add Note 5: ', notes.splice(1, 0, 'Note 5'));
+
+console.log('Splice add Note 5: ', notes);
+console.log('\r');
+
+// Replace item in array with new value using bracket notation
+console.log('Bracket replace: ', notes[2] = 'Note 6');
+
+console.log('Bracket replace: ', notes);
+console.log('\r');
+
+// Add item to array using bracket notation
+console.log('Bracket replace: ', notes[3] = 'Note 7');
+
+console.log('Bracket replace: ', notes);
+console.log('\r');
+
+// Loop through array using forEach method
+notes.forEach(function(item, idx){
+    console.log(`${idx}: ${item}`);
+});
--- js-bootcamp/arrays/todo.js
+++ js-bootcamp/arrays/todo.js
@@ -1,9 +1,38 @@
+// Challenge 1:
 // Create an array with five todos
 // Print message: You have x todos (x is the length of array)
 // Print the first and second to last itmes -> Todo: walk the dog
 
+// Challenge2:
+// Delete the 3rd item
+// Add a new item onto the end
+// Remove the first item from the list
+
+// Challenge 3:
+/*
+
+Print the following:
+
+1. First item of array
+2. Second item of array
+and so on
+
+*/
+
 const todos = ['study JavaScript', 'finish Sass exercise', 'complete SVG exercise', 'feed the monkey', 'beat mega man x9'];
 
+todos.splice(2, 1);
+todos.push('do 50 jumping jax');
+todos.shift();
+
+console.log(todos);
 console.log(`You have ${todos.length} todos!`);
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`)
+
+console.log('\r');
+
+todos.forEach(function(todo, idx) {
+    console.log(`${idx + 1}. ${todo}`)
+});
+
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);

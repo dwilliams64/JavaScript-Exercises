46a4cb63d408f1bc696b293a6558bde7b84f51da
dwilliams64 <dwilliams_64@yahoo.com>
2019-11-20 16:36:47 -0600

    completed hangman challenge 2

--- js-bootcamp/oop/hangman.js
+++ js-bootcamp/oop/hangman.js
@@ -14,7 +14,7 @@
 const Hangman = function(word, tries) {
     this.word = word.toLowerCase().split('');
     this.tries = tries;    
-    this.guessLetters = ['d', 'o'];
+    this.guessLetters = [];
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -26,23 +26,46 @@ Hangman.prototype.getPuzzle = function () {
         } else {
             puzzle += '*';
         }
-    });
-
-    
-
-    
-
-    console.log(puzzle)
+    });    
 
     return puzzle;
 }
 
+Hangman.prototype.getGuess = function (guess) {
+    guess.toLowerCase();
+
+    // Could also use indexOf
+    const isUnique = !this.guessLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess);
+
+    if (isUnique) {
+        this.guessLetters.push(guess);
+    }
+
+    if (isUnique && isBadGuess) {
+        this.tries--;
+    }
+}
+
 const game1 = new Hangman('Dog', 2);
+// Guess d, g, f
+game1.getGuess('d');
+game1.getGuess('g');
+game1.getGuess('f');
+
+game1.getGuess('d');
+game1.getGuess('g');
+game1.getGuess('f');
+
+console.log(game1.getPuzzle()); // d*g
+
+// Print remaing guesses (should be 1)
+console.log(game1.tries);
 
 const game2 = new Hangman('New Jersey', 4);
 
 const game3 = new Hangman('Donkey Kong', 4);
 
-console.log(game1.getPuzzle());
+
 // console.log(game2.getPuzzle());
 // console.log(game3.getPuzzle());

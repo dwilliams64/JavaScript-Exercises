f0189cbfc23a4847555e5695db3597dd89cf682c
dwilliams64 <dwilliams_64@yahoo.com>
2019-11-23 16:20:50 -0600

    Added class.js

---
+++ js-bootcamp/oop/class.js
@@ -0,0 +1,35 @@
+// Works the same way as setting up a constructor function and prototypes
+// It just looks different
+
+class Person {
+
+    // Our Constructor function
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Prototype methods
+    getBio() {
+        let bio = `${this.firstName} is ${this.age} years old.`
+
+        return bio;        
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' '); // Splits name into an array ( ['Jill', 'Valintine'] )
+        this.firstName = names[0]; // Jill
+        this.lastName = names[1]; // Valintine
+    }
+}
+
+const person1 = new Person('Chris', 'Redfield', 45);
+
+// console.log(person1)
+
+console.log(person1.getBio()) // Chris is 45 years old.
+
+person1.setName('Jill Valintine') // Sets firstName to Jill and lastName to Valintine
+
+console.log(person1.firstName) // Jill

db9557a12a12ec2a9b38b31d8d81ce6bedf37171
dwilliams64 <dwilliams_64@yahoo.com>
2019-11-29 09:52:23 -0600

    Restructured folders and files, installed babel and live-server locally, and added npm scripts for babel live-server

--- js-bootcamp/boilerplate/input.js
+++
@@ -1,9 +0,0 @@
-const name = 'Chris Redfield';
-
-console.log(name);
-
-class Hangman {
-    myMethod() {
-        return 'Testing'
-    }
-}
---
+++ js-bootcamp/boilerplate/src/index.js
@@ -0,0 +1,13 @@
+const name = 'Jill Valintine'
+
+console.log(name)
+
+class Hangman {
+    myMethod() {
+        return 'Testing'
+    }
+}
+
+const hangman = new Hangman()
+
+console.log(hangman.myMethod())

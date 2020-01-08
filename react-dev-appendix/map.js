6151450fcc4f003ec6885fdb0c87395a6ece3322
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-08 12:53:58 -0600

    Completed map exercise

---
+++ react-dev-appendix/map.js
@@ -0,0 +1,21 @@
+// The map array method allows us to perform an operation on each
+// individual array element
+// This method returns a brand new array
+
+const myArray = [1, 2, 3, 4];
+
+// map method takes the elements of the array as arguments
+
+let newArray = myArray.map(elm => elm + 2);
+
+// Old array is not mutated
+console.log(myArray);
+
+// New returned array
+console.log(newArray);
+
+// We can also just re-map elements in an array without passing an element as an argument
+let otherArray = myArray.map(() => "b");
+
+// returns [ 'b', 'b', 'b', 'b' ]
+console.log(otherArray);

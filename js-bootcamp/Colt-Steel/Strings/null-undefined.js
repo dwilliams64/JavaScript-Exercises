104bcfb56f5fab0ac55270674f8e49cc830fef2a
dwilliams64 <dwilliams_64@yahoo.com>
2020-06-12 15:05:29 -0500

    Completed null and undefined

---
+++ js-bootcamp/Colt-Steel/Strings/null-undefined.js
@@ -0,0 +1,20 @@
+//////// Null and Undefined /////////////////
+
+// Null
+// - Intentional absence of any value
+// - Must be assigned
+
+// Undefined
+//  - Variables that do not have an assigned value are undefined
+
+// Null Example
+
+let user = null;
+
+console.log(user);
+
+// Undefined Example
+
+let dog;
+
+console.log(dog);

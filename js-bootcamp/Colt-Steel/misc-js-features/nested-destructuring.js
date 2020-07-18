6ac31adfaee04b69cd4dd99c4bff7359cdc725a1
dwilliams64 <dwilliams_64@yahoo.com>
2020-07-18 11:43:54 -0500

    added destructuring

---
+++ js-bootcamp/Colt-Steel/misc-js-features/nested-destructuring.js
@@ -0,0 +1,48 @@
+const results = [{
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya",
+  },
+  {
+    first: 'Feyisa',
+    last: 'Lilesa',
+    country: 'Ethiopia'
+  },
+  {
+    first: 'Galen',
+    last: 'Rupp',
+    country: 'United States'
+  }
+]
+// NESTED DESTRUCTURING
+const [{
+  first: goldWinner
+}, {
+  country
+}] = results;
+goldWinner; //"Eliud"
+country; //"Ethiopia"
+
+const results = [
+  {
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya",
+  },
+  {
+    first: 'Feyisa',
+    last: 'Lilesa',
+    country: 'Ethiopia'
+  },
+  {
+    first: 'Galen',
+    last: 'Rupp',
+    country: 'United States'
+  }
+]
+
+const [, , us] = results;
+
+const { last } = us;
+
+console.log(last); // 'Rupp'

3b786d67f99ff31acdc06f326808c35bdd57002e
dwilliams64 <dwilliams_64@yahoo.com>
2020-08-04 20:05:22 -0500

    added import export challenge

---
+++ scrimba/front-end-career/module 6/Next level JavaScript/import export challenge/data.js
@@ -0,0 +1,4 @@
+// Note: Webpack must be installed to use the module system. this will not work!!!
+
+export const add = (a, b) => a + b;
+
---
+++ scrimba/front-end-career/module 6/Next level JavaScript/import export challenge/index.js
@@ -0,0 +1,7 @@
+// Note: Webpack must be installed to use the module system. this will not work!!!
+
+import { add } from './data.js';
+
+let result = add(2, 2);
+
+console.log(result);

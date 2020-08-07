dac402a737749abef1b9ad6d2965d47a3a545fc3
dwilliams64 <dwilliams_64@yahoo.com>
2020-08-06 20:13:51 -0500

    Modified interface, added conversion functionality, added type checking for input, added disabling depending on data type

---
+++ scrimba/front-end-career/module 4/JavaScript Challenges/temp converter/index.js
@@ -0,0 +1,74 @@
+const inputLabel = document.getElementById("input-label");
+const inputTemp = document.getElementById("input-temp");
+const resultLabel = document.getElementById("result-label");
+const resultTemp = document.getElementById("result-temp");
+const convertTemps = document.getElementById("convert-temps");
+
+const inputChk = (input) => isNaN(input) || input === "";
+
+const disable = () => {
+    convertTemps.setAttribute("disabled", "");
+    convertTemps.classList.add("disable-select");
+    resultTemp.textContent = "Please enter valid number."
+}
+
+const enable = () => {
+    convertTemps.removeAttribute("disabled");
+    convertTemps.classList.remove("disable-select");    
+}
+
+const convert = (value) => {
+    switch (convertTemps.value) {
+        case "f-to-c":
+            inputLabel.textContent = "Fahrenheit";
+            resultLabel.textContent = "Celsius";
+            resultTemp.textContent = `${(value - 32) / 1.8}`;
+            break;
+        case "c-to-f":
+            inputLabel.textContent = "Celsius";
+            resultLabel.textContent = "Fahrenheit";            
+            resultTemp.textContent = `${(value * 1.8) + 32}`;
+            break;
+        case "f-to-k":
+            inputLabel.textContent = "Fahrenheit";
+            resultLabel.textContent = "Kelvin";
+            // (T(°F) + 459.67)× 5/9            
+            resultTemp.textContent = `${(value + 459.67) * (5/9)}`;
+            break;
+        case "c-to-k":
+            inputLabel.textContent = "Celsius";
+            resultLabel.textContent = "Kelvin"; 
+            // T(°C) + 273.15                      
+            resultTemp.textContent = `${value + 273.15}`;
+            break;
+        case "k-to-f":
+            inputLabel.textContent = "Kelvin";
+            resultLabel.textContent = "Fahrenheit";
+            // T(K) × 9/5 - 459.67
+            resultTemp.textContent = `${(value * (9/5)) - 459.67}`;
+            break;
+        case "k-to-c":
+            inputLabel.textContent = "Kelvin";
+            resultLabel.textContent = "Celsius";
+            // T(K) - 273.15
+            resultTemp.textContent = `${value - 273.15}`;
+            break;
+        defualt:
+            console.log("fuck-you")
+    }
+}
+
+
+inputTemp.addEventListener("input", function (e) {
+    if (inputChk(e.target.value)) {
+        disable();
+    } else {
+        enable();
+        convert(e.target.value);
+    }
+
+});
+
+convertTemps.addEventListener("input", function () {
+    convert(inputTemp.value);
+});

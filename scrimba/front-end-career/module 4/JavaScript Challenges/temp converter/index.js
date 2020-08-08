01db3a8fb4aac3878fc4bea6b042c99c8d07cae1
dwilliams64 <dwilliams_64@yahoo.com>
2020-08-08 17:05:42 -0500

    Added rounding to 2 decimal places for conversions

--- scrimba/front-end-career/module 4/JavaScript Challenges/temp converter/index.js
+++ scrimba/front-end-career/module 4/JavaScript Challenges/temp converter/index.js
@@ -22,36 +22,36 @@ const convert = (value) => {
         case "f-to-c":
             inputLabel.textContent = "Fahrenheit";
             resultLabel.textContent = "Celsius";
-            resultTemp.textContent = `${(value - 32) / 1.8}`;
+            resultTemp.textContent = `${((value - 32) / 1.8).toFixed(2)}`;            
             break;
         case "c-to-f":
             inputLabel.textContent = "Celsius";
             resultLabel.textContent = "Fahrenheit";            
-            resultTemp.textContent = `${(value * 1.8) + 32}`;
+            resultTemp.textContent = `${((value * 1.8) + 32).toFixed(2)}`;
             break;
         case "f-to-k":
             inputLabel.textContent = "Fahrenheit";
             resultLabel.textContent = "Kelvin";
             // (T(°F) + 459.67)× 5/9            
-            resultTemp.textContent = `${(value + 459.67) * (5/9)}`;
+            resultTemp.textContent = `${((value + 459.67) * (5/9)).toFixed(2)}`;
             break;
         case "c-to-k":
             inputLabel.textContent = "Celsius";
             resultLabel.textContent = "Kelvin"; 
             // T(°C) + 273.15                      
-            resultTemp.textContent = `${value + 273.15}`;
+            resultTemp.textContent = `${(value + 273.15).toFixed(2)}`;
             break;
         case "k-to-f":
             inputLabel.textContent = "Kelvin";
             resultLabel.textContent = "Fahrenheit";
             // T(K) × 9/5 - 459.67
-            resultTemp.textContent = `${(value * (9/5)) - 459.67}`;
+            resultTemp.textContent = `${((value * (9/5)) - 459.67).toFixed(2)}`;
             break;
         case "k-to-c":
             inputLabel.textContent = "Kelvin";
             resultLabel.textContent = "Celsius";
             // T(K) - 273.15
-            resultTemp.textContent = `${value - 273.15}`;
+            resultTemp.textContent = `${(value - 273.15).toFixed(2)}`;
             break;
         defualt:
             console.log("fuck-you")

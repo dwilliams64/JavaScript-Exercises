d9739c2b86e1706fdc968e85cf0ea93abdb22b30
dwilliams64 <dwilliams_64@yahoo.com>
2020-08-09 16:20:21 -0500

    Fixed user interface, limited character entry for input, removed trailing zeros from return value, using correct data types to avoid errors

--- scrimba/front-end-career/module 4/JavaScript Challenges/temp converter/index.js
+++ scrimba/front-end-career/module 4/JavaScript Challenges/temp converter/index.js
@@ -9,7 +9,7 @@ const inputChk = (input) => isNaN(input) || input === "";
 const disable = () => {
     convertTemps.setAttribute("disabled", "");
     convertTemps.classList.add("disable-select");
-    resultTemp.textContent = "Please enter valid number."
+    resultTemp.value = "Please enter valid number."
 }
 
 const enable = () => {
@@ -17,47 +17,44 @@ const enable = () => {
     convertTemps.classList.remove("disable-select");    
 }
 
-const convert = (value) => {
+const convert = (value) => {    
+    value = parseFloat(value);
+
     switch (convertTemps.value) {
         case "f-to-c":
             inputLabel.textContent = "Fahrenheit";
-            resultLabel.textContent = "Celsius";
-            resultTemp.textContent = `${((value - 32) / 1.8).toFixed(2)}`;            
+            resultLabel.textContent = "Celsius";            
+            resultTemp.value = parseFloat(((value - 32) / 1.8).toFixed(2));                       
             break;
         case "c-to-f":
             inputLabel.textContent = "Celsius";
             resultLabel.textContent = "Fahrenheit";            
-            resultTemp.textContent = `${((value * 1.8) + 32).toFixed(2)}`;
+            resultTemp.value  = parseFloat(((value * 1.8) + 32).toFixed(2));
             break;
         case "f-to-k":
             inputLabel.textContent = "Fahrenheit";
-            resultLabel.textContent = "Kelvin";
-            // (T(°F) + 459.67)× 5/9            
-            resultTemp.textContent = `${((value + 459.67) * (5/9)).toFixed(2)}`;
+            resultLabel.textContent = "Kelvin";            
+            resultTemp.value  = parseFloat(((value + 459.67) * (5/9)).toFixed(2));
             break;
         case "c-to-k":
             inputLabel.textContent = "Celsius";
-            resultLabel.textContent = "Kelvin"; 
-            // T(°C) + 273.15                      
-            resultTemp.textContent = `${(value + 273.15).toFixed(2)}`;
+            resultLabel.textContent = "Kelvin";                                               
+            resultTemp.value  = parseFloat((value + 273.15).toFixed(2));
             break;
         case "k-to-f":
             inputLabel.textContent = "Kelvin";
-            resultLabel.textContent = "Fahrenheit";
-            // T(K) × 9/5 - 459.67
-            resultTemp.textContent = `${((value * (9/5)) - 459.67).toFixed(2)}`;
+            resultLabel.textContent = "Fahrenheit";            
+            resultTemp.value  = formatNum(((value * (9/5)) - 459.67).toFixed(2));
             break;
         case "k-to-c":
             inputLabel.textContent = "Kelvin";
-            resultLabel.textContent = "Celsius";
-            // T(K) - 273.15
-            resultTemp.textContent = `${(value - 273.15).toFixed(2)}`;
+            resultLabel.textContent = "Celsius";            
+            resultTemp.value  = formatNum((value - 273.15).toFixed(2));
             break;
         defualt:
             console.log("fuck-you")
     }
 }
-
 
 inputTemp.addEventListener("input", function (e) {
     if (inputChk(e.target.value)) {

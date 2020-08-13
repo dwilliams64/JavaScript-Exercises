20d92201193a67c7fd229743ccda35c3d74db71b
dwilliams64 <dwilliams_64@yahoo.com>
2020-08-12 19:34:09 -0500

    Added javascript exercises for Javascipt bootcamp

---
+++ scrimba/JavaScript BootCamp/Variables and Strings/variable basics challenge/instructor.js
@@ -0,0 +1,13 @@
+// Fix the variable names below, and log them out to the console!
+
+var firstName = "Reed";
+
+var surName = "Barger";
+
+var oneThousand = "one thousand";
+
+console.log(firstName);
+console.log(surName);
+console.log(oneThousand);
+
+// Need help? Check out the hint.js file
---
+++ scrimba/JavaScript BootCamp/Variables and Strings/variable basics challenge/solution.js
@@ -0,0 +1,14 @@
+// Fix the variable names below, and log them out to the console!
+
+var firstName = "Reed";
+
+var surName = "Barger";
+
+var _1000 = "one thousand";
+
+
+// Need help? Check out the hint.js file
+
+console.log(firstName);
+console.log(surName);
+console.log(_1000);
